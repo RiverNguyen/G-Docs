198159f39eab451eeb406077c8bb270f33785260
Giang Nguyen Dinh <ngiang1309@gmail.com>
2025-02-04 22:23:01 +0700

    INSTALL: Titpap Image

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -3,6 +3,12 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import TaskItem from "@tiptap/extension-task-item";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
 import TaskList from "@tiptap/extension-task-list";
 
 export const Editor = () => {
@@ -20,8 +26,27 @@ export const Editor = () => {
       TaskItem.configure({
         nested: true,
       }),
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      Image,
+      ImageResize,
     ],
-    content: "<p>Hello World! 🌎️</p>",
+    content: `<table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>`,
   });
 
   return (
