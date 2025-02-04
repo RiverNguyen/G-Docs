7adcf0971992299181edef574fd2826fd386a09a
RiverNguyen <ngiang1309@gmail.com>
2025-02-04 12:57:01 +0700

    INSTALL: Tiptap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-red-500">
       <p>Hello World</p>
+      <Button variant={"link"}>Click me</Button>
     </div>
   );
 };
