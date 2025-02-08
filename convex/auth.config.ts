8a758601588a7dc5b13e81ab4f516d5ec5a03f6c
Giang Nguyen Dinh <ngiang1309@gmail.com>
2025-02-09 02:52:19 +0700

    ADD: Integrate Clerk authentication and add fullscreen loader component

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://ace-ant-79.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { SearchInput } from "./search-input";
+import { UserButton } from "@clerk/nextjs";
 
 export const Navbar = () => {
   return (
@@ -13,7 +14,7 @@ export const Navbar = () => {
         <h3 className="text-xl">Docs</h3>
       </div>
       <SearchInput />
-      <div />
+      <UserButton />
     </nav>
   );
 };
